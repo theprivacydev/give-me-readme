d4bb93c5cf34c8b0097973a075062acd6a9c9212
Pro-Code-art <jayme.coder@gmail.com>
2021-01-18 15:48:37 -0800

    added questions and updated writeToFile function

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
     {
         type: 'input',
@@ -35,17 +35,33 @@ const questions = [
         name: 'credits',
       },
       {
-        type: 'input',
+        type: 'list',
         message: 'What is the license for this application?',
         name: 'license',
+        choices: [
+            'MIT',
+            'ISC',
+            'GNU GPLv3',
+            'Apache License 2.0',
+        ],
+      },
+      {
+        type: 'input',
+        message: 'What would you like to list as next steps for this application?',
+        name: 'nextSteps',
+      },
+      {
+        type: 'input',
+        message: 'Who would you like to list as the author and what is their contact info?',
+        name: 'author',
       },
 
 ];
 
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
     console.log(data); 
-    fs.writeFile(fileName, `Title: ${data.title} \nDescription: ${data.description} \nTable of Contents: ${data.contents} \nInstallation: ${data.installation} \n Installation: ${data.installation} \nUsage: ${data.usage} \nCredits: ${data.credits} \nLicense: ${data.license}`, (err) =>
+    fs.writeFile(fileName, `Title: ${data.title} \nDescription: ${data.description} \nTable of Contents: ${data.contents} \nInstallation: ${data.installation} \n Installation: ${data.installation} \nUsage: ${data.usage} \nCredits: ${data.credits} \nLicense: ${data.license} \nNext Steps: ${data.nextSteps} \nAuthor: ${data.author}`, (err) =>
         err ? console.error(err) : console.log('Success!')
     );
 }
